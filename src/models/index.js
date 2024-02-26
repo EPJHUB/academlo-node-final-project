a033835dc2e666b2e619b6fa4fb58382a7467cd8
ErickPatiñoMac <pj_erick@hotmail.com>
2024-02-25 22:51:28 -0600

    purchase MVC finished

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,23 @@
 const User = require("./User");
 const Category = require("./Category");
 const Product = require("./Product");
+const Cart = require("./Cart");
+const Purchase = require("./Purchase");
 
 
 Product.belongsTo(Category) //-> categoryId
 Category.hasMany(Product)
+
+Cart.belongsTo(User) // -> userId
+User.hasMany(Cart)
+
+Cart.belongsTo(Product) // productId
+Product.hasMany(Cart)
+
+Purchase.belongsTo(User)
+User.hasMany(Purchase)
+
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
+
+
